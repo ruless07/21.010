c312bab4444eff6b22b83ff7b2e898682d74e903
Bucha Sergey <donotmeforus.gmail.com>
2016-10-24 20:25:31 +0300

    first save

---
+++ myJs.js
@@ -0,0 +1,32 @@
+//radiobutton
+ $("#group1").buttonset();
+
+ //modal windows answer
+
+ $('#answerAdd').dialog(
+ {autoOpen:false},
+ {width:'600px'});
+// this is my comment
+//button
+var massA=[];
+//constructor obA
+function AnswerObj(text) {
+  this.text=text;
+}
+var textA=function (){
+var  text=$('#textAnswer').text();
+alert(text);
+return text;
+}
+
+
+//
+
+
+function plass2(){
+  var newAnswer=new AnswerObj(textA);
+  if ("text" in newAnswer) {
+  alert( "Свойство name существует!" );
+}
+$('#answerAdd').dialog('close');
+}
