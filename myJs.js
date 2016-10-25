e9a99a98f5a123680a20d2b98f5813930c25bd59
Bucha Sergey <donotmeforus.gmail.com>
2016-10-25 23:56:22 +0300

    в  свойствах обьекта ---функции а не переменные

--- myJs.js
+++ myJs.js
@@ -1,32 +1,49 @@
 //radiobutton
  $("#group1").buttonset();
-
- //modal windows answer
-
- $('#answerAdd').dialog(
+//modal windows answer
+$('#answerAdd').dialog(
  {autoOpen:false},
  {width:'600px'});
-// this is my comment
-//button
-var massA=[];
-//constructor obA
-function AnswerObj(text) {
-  this.text=text;
+
+function dialogOpen(){
+  $('#answerAdd').dialog('open')
 }
+
+
+
 var textA=function (){
 var  text=$('#textAnswer').text();
-alert(text);
 return text;
 }
+var boolA=function(){
+    var tf=document.getElementsByName('aTrueFalse');
+    for (var i=0;i<tf.length; i++) {
+        if (tf[i].checked) {
+            alert('Выбран '+i+' radiobutton');
+        }
+    }
+}
+//massiv
+var massA=[];
+//constructor obA
+function AnswerObj(textt,booll) {
+  this.text=textt;
+  this.bool=booll;
+}
+
+
 
 
 //
 
 
 function plass2(){
-  var newAnswer=new AnswerObj(textA);
+  var newAnswer=new AnswerObj(textA,boolA);
   if ("text" in newAnswer) {
-  alert( "Свойство name существует!" );
+//  alert( "Свойство name существует!" );
 }
+massA.push(newAnswer);
+alert(massA[0].text);
+alert(massA[0].bool);
 $('#answerAdd').dialog('close');
 }
