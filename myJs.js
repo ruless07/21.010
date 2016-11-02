2b03d34ca917b32939b4d5d4652b321679d0406b
estbyright <estbyright@gmail.com>
2016-11-02 15:59:37 +0300

    make it work

--- myJs.js
+++ myJs.js
@@ -1,36 +1,60 @@
-//radiobutton
- $("#group1").buttonset();
-//modal windows answer
-$('#answerAdd').dialog(
- {autoOpen:false},
- {width:'600px'});
 
-function dialogOpen(){
-  $('#answerAdd').dialog('open')
-}
-//massiv
-var answers = [];
+
+let answers = [];
+
+let button1Query = $('#button1').first();
+let button3Query = $('#button3').first();
 
 //constructor obA
 function AnswerObj(text, right) {
   this.right = right;
   this.text = text;
-  console.log("created " + this);
+  console.log("created AnswerObj: " + this.right + ": " + this.text);
 }
 
 
-function plass2() {
+//ui setup
+//radiobutton
+ //$("#answerTabBar").buttonset();
 
+//modal windows answer
+$('#answerDialog').dialog(
+ {autoOpen:false},
+ {width:'250px'}
+);
 
-  let text = $('#textAnswer').val();
-  let right = $('input[name=radio]:checked', '#myForm');
-  console.log(right.toString());
-  let answer = new AnswerObj(text, right);
-  answers.push(answer);
-
-  for(var i = 0; i < answers.length; i++) {
-    console.log(answers[i]);
+function listAnswers (localAnswers) {
+  console.log("list of answers:");
+  for(let idx = 0; idx < localAnswers.length; idx++) {
+    let answer = localAnswers[idx];
+    let desc = ""
+    if (answer.right) {
+      desc += "* ";
+    }
+    desc += answer.text;
+    console.log(desc);
   }
+}
 
-  $('#answerAdd').dialog('close');
+if (button1Query.length === 1) {
+  console.log("button1 connected");
+  button1Query.click(function button1_onClick()  {
+    console.log("button1_onClick")
+    $('#answerDialog').first().dialog('open');
+  });
+}
+
+if (button3Query.length === 1) {
+  console.log("button3 connected");
+  button3Query.click(function button3_onClick() {
+    console.log("button3_onClick")
+    let text = $('#textAnswer').val();
+    let checked = $('#rightAnswer').prop('checked');
+    let answer = new AnswerObj(text, checked);
+    answers.push(answer);
+
+    listAnswers(answers);
+
+    $('#answerDialog').dialog('close');
+  });
 }
